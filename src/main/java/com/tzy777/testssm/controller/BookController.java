d4598b17cc4b45e0da3d9721a346726677c74336
tzy521521 <843460950@qq.com>
2018-06-28 16:09:18 +0800

    编写controller

--- src/main/java/com/tzy777/testssm/controller/BookController.java
+++ src/main/java/com/tzy777/testssm/controller/BookController.java
@@ -1,14 +1,18 @@
 package com.tzy777.testssm.controller;
 
+import com.tzy777.testssm.dto.AppointExecution;
+import com.tzy777.testssm.dto.Result;
 import com.tzy777.testssm.entity.Book;
+import com.tzy777.testssm.enums.AppointStateEnum;
+import com.tzy777.testssm.exception.NoNumberException;
+import com.tzy777.testssm.exception.RepeatAppointException;
 import com.tzy777.testssm.service.BookService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +33,39 @@ public class BookController {
         List<Book> list = bookService.getList();
         model.addAttribute("list",list);
         return "list";
+    }
+
+    @RequestMapping(value = "/{bookId}/detail",method = RequestMethod.GET)
+    private String detail(@PathVariable("bookId")Long bookId,Model model){
+        if (bookId==null){
+            return "redirect:/book/list";
+        }else {
+            Book book =bookService.getById(bookId);
+            if (book == null) {
+                return "forward:/book/list";
+            }
+            model.addAttribute("book", book);
+            return "detail";
+        }
+    }
+
+    @RequestMapping(value = "/{bookId}/appoint",method = RequestMethod.POST,produces = {"application/json;charset=utf-8" })
+    @ResponseBody
+    private Result<AppointExecution> appoint(
+            @PathVariable("bookId") Long bookId, @RequestParam("studentId") Long studentId) {
+        if (studentId == null || studentId.toString().equals("")) {
+            return new Result<>(false, "学号不能为空");
+        }
+        AppointExecution execution=null;
+        try {
+            execution = bookService.appoint(bookId,studentId);
+        }catch (NoNumberException e1) {
+            execution = new AppointExecution(bookId, AppointStateEnum.NO_NUMBER);
+        } catch (RepeatAppointException e2) {
+            execution = new AppointExecution(bookId, AppointStateEnum.REPEAT_APPOINT);
+        } catch (Exception e) {
+            execution = new AppointExecution(bookId, AppointStateEnum.INNER_ERROR);
+        }
+        return new Result<AppointExecution>(true, execution);
     }
 }
